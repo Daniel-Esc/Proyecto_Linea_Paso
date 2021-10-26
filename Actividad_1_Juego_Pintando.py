4de95237a15cb799f242b16ad70c8ff0d96886ff
Daniel-Esc <74694850+Daniel-Esc@users.noreply.github.com>
2021-10-26 12:47:00 -0500

    Corrección conflicto entre colores
    Baje el comando una línea para que pudiera hacer merge

--- Actividad_1_Juego_Pintando.py
+++ Actividad_1_Juego_Pintando.py
@@ -59,6 +59,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+
 onkey(lambda: color('gray'), 'A')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
